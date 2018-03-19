889aa8357a0fd455e20349b4103402b803688db2
Massimiliano Masserelli <massimiliano.masserelli@essepi.eu>
2018-03-19 19:43:34 +0100

    Transitional index on frontend for guest users

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -6,14 +6,9 @@ $this->title = Yii::$app->name;
 ?>
 <div class="site-index">
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Panel Manager</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">Applicazione in fase di test, vi preghiamo di segnalare eventuali malfunzionamenti.</p>
 
-        <p>
-            <a href="http://www.yiiframework.com" class="btn btn-lg btn-success">Get started with Yii</a>
-            <a href="https://github.com/Beaten-Sect0r/yii2-core" class="btn btn-lg btn-primary">Yii2 Core on GitHub</a>
-            <a href="https://github.com/Beaten-Sect0r/yii2-core/issues" class="btn btn-lg btn-danger">Find a bug?</a>
-        </p>
     </div>
 </div>
